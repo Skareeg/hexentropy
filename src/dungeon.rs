e9326ba52fcbc456a1c1f33eb5492583dcf8002b
Skareeg <skareeg@gmail.com>
2023-07-04 14:00:33 -0400

    Zoomaloom.

--- src/dungeon.rs
+++ src/dungeon.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use ndarray::{Array3, array};
+
+use crate::{GameAssets, tileset_1bit::TileSet1Bit};
 
 pub const LEVEL_SIZE_X: usize = 128;
 pub const LEVEL_SIZE_Y: usize = 128;
@@ -7,8 +10,22 @@ pub const LVL_S_C: usize = LEVEL_SIZE_X * LEVEL_SIZE_Y * LEVEL_SIZE_Z;
 
 #[derive(Component)]
 pub struct Level {
-    pub tiles: [Entity; LVL_S_C],
+    pub tiles: Array3<Option<Entity>>,
+    pub size: [usize; 3],
 }
+
+impl Default for Level {
+    fn default() -> Self {
+        let size = [LEVEL_SIZE_X, LEVEL_SIZE_Y, LEVEL_SIZE_Z];
+        Self {
+            tiles: Array3::from_shape_simple_fn(size, || None),
+            size,
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct Tile;
 
 #[derive(Component)]
 pub struct Opaque;
@@ -19,5 +36,49 @@ pub struct Solid;
 #[derive(Component)]
 pub struct GenRoom;
 
-pub fn gen_dungeon_floor(mut commands: Commands) {
+#[derive(Bundle)]
+pub struct WallBundle {
+    pub opaque: Opaque,
+    pub solid: Solid,
+    pub texture_bundle: SpriteSheetBundle,
+    // pub texture: Handle<TextureAtlas>,
+    // pub sprite: TextureAtlasSprite,
+    // pub transform: Transform,
+    // pub global: GlobalTransform,
+}
+
+impl WallBundle {
+    pub fn new(position: [usize; 3], atlas: Handle<TextureAtlas>, tile: TileSet1Bit) -> Self {
+        Self { opaque: Opaque, solid: Solid, texture_bundle: SpriteSheetBundle {
+            texture_atlas: atlas,
+            sprite: TextureAtlasSprite::new(tile as usize),
+            transform: Transform::from_xyz(position[0] as f32 * 16.0, position[1] as f32 * 16.0, position[2] as f32 * 16.0),
+            ..default()
+        } }
+    }
+}
+
+pub fn gen_dungeon_floor(mut commands: Commands, assets: Res<GameAssets>, mut levels: Query<(Entity, &mut Level), With<GenRoom>>) {
+    for (ent, mut level) in &mut levels {
+        let i1 = 48usize;
+        let i2 = 78usize;
+        let j1 = 48usize;
+        let j2 = 70usize;
+        let k = 4usize;
+        // Create a small test room to play in.
+        for i in i1..=i2 {
+            let pos1 = [i, j1, k];
+            let pos2 = [i, j2, k];
+            level.tiles[pos1] = Some(commands.spawn(WallBundle::new(pos1, assets.atlas1.clone(), TileSet1Bit::BlockX)).id());
+            level.tiles[pos2] = Some(commands.spawn(WallBundle::new(pos2, assets.atlas1.clone(), TileSet1Bit::BlockX)).id());
+        }
+        for j in j1+1..j2 {
+            let pos1 = [i1, j, k];
+            let pos2 = [i2, j, k];
+            level.tiles[pos1] = Some(commands.spawn(WallBundle::new(pos1, assets.atlas1.clone(), TileSet1Bit::BlockX)).id());
+            level.tiles[pos2] = Some(commands.spawn(WallBundle::new(pos2, assets.atlas1.clone(), TileSet1Bit::BlockX)).id());
+        }
+        commands.entity(ent).remove::<GenRoom>();
+        println!("Dungeon generated test room.")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 use bevy::{prelude::*, app::AppExit};
+use bevy_embedded_assets::EmbeddedAssetPlugin;
+use bevy_rapier3d::{prelude::{RapierPhysicsPlugin, NoUserData}, render::RapierDebugRenderPlugin};
+use dungeon::gen_dungeon_floor;
+
+use crate::dungeon::{Level, GenRoom};
 
 pub mod tileset_1bit;
 pub mod physics;
 
 pub mod dungeon;
+pub mod player;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .init_resource::<GameAssets>()
-        .add_plugins(DefaultPlugins.set(
-            ImagePlugin::default_nearest(),
-        ))
+        .add_plugins(DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .build()
+            .add_before::<AssetPlugin, _>(EmbeddedAssetPlugin)
+        )
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default().with_physics_scale(16.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
         .add_state::<AppState>()
         .add_system(load_textures.in_schedule(OnEnter(AppState::Setup)))
         .add_system(check_tileset_asset.in_set(OnUpdate(AppState::Setup)))
         .add_system(setup.in_schedule(OnEnter(AppState::Run)))
+        .add_system(zoomout)
+        .add_system(gen_dungeon_floor)
         .run();
 }
 
@@ -41,7 +53,7 @@ pub struct GameAssets {
 
 fn load_textures(mut commands: Commands, mut assets: ResMut<GameAssets>, mut atlases: ResMut<Assets<TextureAtlas>>, sets: Res<AssetServer>) {
     let texture = sets.load("tileset.png");
-    let tileset = atlases.add(TextureAtlas::from_grid(texture.clone(), Vec2::new(16., 16.), 49, 22, Some(Vec2::new(1., 1.)), None));
+    let tileset = atlases.add(TextureAtlas::from_grid(texture.clone(), Vec2::new(16., 16.), 49, 22, None, None));
     assets.atlas1_img = texture;
     assets.atlas1 = tileset;
     println!("Loading...");
@@ -68,19 +80,19 @@ fn check_tileset_asset(mut next_state: ResMut<NextState<AppState>>, assets: Res<
     }
 }
 
-fn coord_ind(x: usize, y: usize, mx: usize) -> usize {
-    x + (mx * y)
-}
-
 fn setup(mut commands: Commands, assets: Res<GameAssets>, atlases: Res<Assets<TextureAtlas>>) {
     println!("Spawning...");
     let atlas = atlases.get(&assets.atlas1).unwrap();
-    commands.spawn(Camera2dBundle::default());
+    let mut cam = Camera2dBundle::new_with_far(100.0 * 16.0);
+    cam.transform.translation.x = 50.0 * 16.0;
+    cam.transform.translation.y = 50.0 * 16.0;
+    cam.projection.scale = 1.5;
+    commands.spawn(cam);
     commands.spawn((
         SpriteSheetBundle {
             texture_atlas: assets.atlas1.clone(),
             sprite: TextureAtlasSprite::new(tileset_1bit::TileSet1Bit::Human as usize),
-            transform: Transform::from_xyz(-500., 0., 0.),
+            transform: Transform::from_xyz(50.0 * 16.0, 50.0 * 16.0, 0.),
             ..default()
         },
     ));
@@ -91,5 +103,16 @@ fn setup(mut commands: Commands, assets: Res<GameAssets>, atlases: Res<Assets<Te
             ..default()
         },
     ));
+    commands.spawn((
+        Level::default(),
+        GenRoom,
+    ));
     println!("Ready.");
+}
+
+fn zoomout(mut cams: Query<&mut OrthographicProjection, With<Camera>>, time: Res<Time>) {
+    use lerp::Lerp;
+    for mut cam in &mut cams {
+        cam.scale = cam.scale.lerp(cam.scale / 1.0002, time.elapsed_seconds());
+    }
 }
